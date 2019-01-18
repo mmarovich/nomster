578a63857b65a6a1d6556489ba5f67bca35b0223
mmarovich <mmarovich@gmail.com>
2019-01-18 18:47:28 -0500

    added kaminari pagination

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,7 @@
 class PlacesController < ApplicationController
 
   def index
-    @places = Place.all
+    @places = Place.all.order(:name)
+    @places = Kaminari.paginate_array(@places).page(params[:page]).per(5)
   end
 end
