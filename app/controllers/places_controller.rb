2431959911cb9b6d1dbf888679c3551606dab790
mmarovich <mmarovich@gmail.com>
2019-01-18 20:31:09 -0500

    added form to add new places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -4,4 +4,8 @@ class PlacesController < ApplicationController
     @places = Place.all.order(:name)
     @places = Kaminari.paginate_array(@places).page(params[:page]).per(5)
   end
+
+  def new
+    @place = Place.new
+  end
 end
