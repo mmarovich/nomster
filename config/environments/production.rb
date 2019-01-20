6328554302adc793726fdc378e000c3161159192
mmarovich <mmarovich@gmail.com>
2019-01-20 15:18:22 -0500

    added and set up devise gem for user login

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,6 @@
 Rails.application.configure do
+  # default url options defined for devise
+  config.action_mailer.default_url_options = { host: 'https://nomster-maxwell-marovich.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
