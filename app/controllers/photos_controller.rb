2b35c15084beda47024d24decf78f8feab0cb73b
mmarovich <mmarovich@gmail.com>
2019-02-05 17:50:38 -0500

    set up photo database and views

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,15 +1,15 @@
-class CommentsController < ApplicationController
+class PhotosController < ApplicationController
   before_action :authenticate_user!
 
   def create
     @place = Place.find(params[:place_id])
-    @place.photos.create(photo_params.merge(user: current_user))
+    @place.photos.create(photo_params)
     redirect_to place_path(@place)
   end
 
   private
 
   def photo_params
-    params.require(:photo).permit(:image, :description)
+    params.require(:photo).permit(:picture, :caption)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -27,14 +27,12 @@ ActiveRecord::Schema.define(version: 20190130203832) do
   end
 
   create_table "photos", force: :cascade do |t|
-    t.text     "description"
+    t.text     "caption"
     t.integer  "place_id"
-    t.integer  "user_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.string   "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "picture"
     t.index ["place_id"], name: "index_photos_on_place_id", using: :btree
-    t.index ["user_id", "place_id"], name: "index_photos_on_user_id_and_place_id", using: :btree
   end
 
   create_table "places", force: :cascade do |t|
